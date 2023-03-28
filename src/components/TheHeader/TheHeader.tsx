2d430c7575ff7b88daff1e13f734e2e3f6d86ede
Volodymyr-Trygubiak <trigubiak@gmail.com>
2023-03-28 23:03:56 +0200

    Add base component

---
+++ src/components/TheHeader/TheHeader.tsx
@@ -0,0 +1,14 @@
+import { ThemeSwitcher } from 'components/ThemeSwitcher';
+
+
+import styles from './TheHeader.module.scss';
+
+
+export const TheHeader = () => (
+  <div className={styles.header}>
+    <div className={styles.logo}>
+      Definder
+    </div>
+    <ThemeSwitcher />
+  </div>
+);
