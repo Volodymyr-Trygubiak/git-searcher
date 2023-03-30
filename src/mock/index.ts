271bf1260709030be09514640ab3bafbbbe057d9
Volodymyr-Trygubiak <trigubiak@gmail.com>
2023-03-30 13:07:49 +0200

    created user card and added user stat cpomponent

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types"
+
+export const defaultUser: LocalGithubUser = {
+  login: 'string',
+  id: 2243,
+  avatar: 'string',
+  name: "string",
+  company: "string",
+  blog: "string",
+  location: "string",
+  bio: "string",
+  twitter: "string",
+  repos: "string",
+  followers: 1,
+  following: 1,
+  creater: "string"
+}
